8f1c6814120c9888195ea8d12eba6211d95c4525
Manyata Dhakal <manyatadhakal12@gmail.com>
2026-09-05 14:29:56 +0545

    Add product loading and error states

--- src/components/Products.js
+++ src/components/Products.js
@@ -2,17 +2,40 @@ import { useState, useEffect} from "react";
 import ProductCard from "./ProductCard";
 function Products(){
     const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
 
     useEffect(() => {
         fetch('https://dummyjson.com/products')
-        .then(response => response.json())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Failed to fetch products');
+            }
+            return response.json();
+        })
         .then(data => {
             setProducts(data.products);
+            setLoading(false);
+        })
+        .catch((error) => {
+            setError(true);
+            setLoading(false);
         });
     }, []);
     return (
         <>
         {
+            loading ?
+            <h2>Loading products...</h2>
+            :
+            error
+            ?
+            <h2>Failed to load products.</h2>
+            :
+            products.length === 0
+            ?
+            <h2>No products found.</h2>
+            :
             products.map((product)=> {
                 return (
                     <ProductCard
