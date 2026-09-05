86e18465af919bfd7c022151f695935bd4216f36
Manyata Dhakal <manyatadhakal12@gmail.com>
2026-09-05 21:07:23 +0545

    Add cart item count to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,13 @@
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 function Navbar(){
+
+    const cart = useSelector((state) => state.cart);
     return(
         <nav>
             <Link to="/">Products</Link>
             { ' | '}
-            <Link to="/cart">Cart</Link>
+            <Link to="/cart">Cart ({cart.length})</Link>
         </nav>
     );
 }
