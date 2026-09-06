2ecbe4fb636f9c8b822ee138b71557ed97a6aef8
Manyata Dhakal <manyatadhakal12@gmail.com>
2026-09-06 11:49:24 +0545

    Style navbar and add global theme

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,21 @@
-import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-function Navbar(){
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+function Navbar() {
+  const cart = useSelector((state) => state.cart);
+  return (
+    <div className="navbar">
+      <div className="nav-container">
+        <Link to="/" className="logo">
+          MiniStore
+        </Link>
 
-    const cart = useSelector((state) => state.cart);
-    return(
-        <nav>
-            <Link to="/">Home</Link>
-            {' | '}
-            <Link to="/products">Products</Link>
-            { ' | '}
-            <Link to="/cart">Cart ({cart.length})</Link>
-        </nav>
-    );
+        <div className="nav-links">
+          <Link to="/">Home</Link>
+          <Link to="/products">Products</Link>
+          <Link to="/cart">Cart ({cart.length})</Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default Navbar;
